d1e7fd1b426eef924f25773274610cefed06e580
Dan Nelson <dnelson@cybertooth.ca>
2017-01-15 14:01:01 -0700

    Document and comment.

--- component-dot-to-slashes.rb
+++ component-dot-to-slashes.rb
@@ -1,18 +1,22 @@
-files = File.join('frontend', '{app,tests}', '**', '*.{hbs,js}')
+# make sure to place this script at the root of your emberjs application; same level as your package.json
+files = File.join('{app,tests}', '**', '*.{hbs,js}')
 
 Dir.glob(files) do |file_name|
   puts "Scanning #{file_name}"
   contents = File.read(file_name)
+  # scan the js/hbs file using the following regex (tested here https://regex101.com/r/KaPP9l/8)
   to_replace = contents.scan(/\{\{[\#|\/]?[a-z]*[\-]+[a-z|\-|\.]*/)
 
+  # if no dot-notation components or helpers are found move on to the next file
   next if to_replace.empty?
 
   new_contents = String.new(contents)
+  # for each component/helper with dot-notation, turn it to slash-notation and replace in a copy of the file's contents
   to_replace.each do |dot_name|
     slash_name = dot_name.gsub('.', '/')
-    puts "Replacing: #{dot_name} => #{slash_name}"
+    puts "\tReplacing: #{dot_name} => #{slash_name}"
     new_contents = new_contents.gsub(dot_name, slash_name)
   end
-  # puts new_contents
+  # save the file with the altered contents
   File.open(file_name, 'w') { |file| file.puts new_contents }
 end
